8014f1244b17e409c99bce5b3bb92c4672ffdc2c
Dragos Geornoiu <dgeornoiu@luxoft.com>
2020-11-02 15:33:27 +0200

    First test.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,7 +4,9 @@
  *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
  *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
  */
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -18,15 +20,24 @@ import org.junit.Test;
 public class BowlingGameTest {
 
     //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Instance fields 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    private BowlingGame game;
+
+    //~ ----------------------------------------------------------------------------------------------------------------
     //~ Methods 
     //~ ----------------------------------------------------------------------------------------------------------------
 
+    @Before
+    public void SetUp() {
+        game = new BowlingGame();
+    }
+
     @Test
-    public void test1() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
-        Assert.assertEquals(0, game.score());
+    public void TestTwoThrowsNoMark() {
+        game.roll(5);
+        game.roll(4);
+        Assert.assertEquals(9, game.score());
     }
 }
